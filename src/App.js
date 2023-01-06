bfc62e2a56e19bd706f3611996732721fd60c97d
Saradi Roy <saradiroy82@gmail.com>
2023-01-06 16:58:13 +0530

    Added ToDo App Frontend File

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import ToDo from './components/ToDo';
+import { getAllToDo, addToDo, updateToDo, deleteToDo } from './components/utils/HandleAPI';
+
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState(" ");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [toDoId, setToDoId] = useState(" ");
+
+  useEffect(() => {
+    getAllToDo(setToDo);
+  }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setText(text);
+    setToDoId(_id)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>ToDo App</h1>
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Enter ToDos....."
+            value={text}
+            onChange={(e) => setText(e.target.value)} />
+
+          <div className="add"
+            onClick={isUpdating ?
+              () => updateToDo(toDoId, text, setText, setToDo, setIsUpdating)
+              : () => addToDo(text, setText, setToDo)}>
+            {isUpdating ? "Update" : "Add"}</div>
+        </div>
+
+        <div className="list">
+          {
+            toDo.map((item) => {
+              return <ToDo key={item._id}
+                text={item.text}
+                updateMode={() => updateMode(item._id, item.text)}
+                deleteToDo={() => deleteToDo(item._id, setToDo)} />
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
